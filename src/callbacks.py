cc2b1d3b15b4f9344449ba32f21ba76c8069d7ee
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-05-02 17:51:34 +0200

    Add weather emoji to today_weather callback

--- src/callbacks.py
+++ src/callbacks.py
@@ -2,10 +2,11 @@ from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
 from telegram.helpers import escape_markdown
 
+from constants import ICON_ID_TO_EMOJI
 from weather_services import retrieve_weather_info
 
 weather_today_template = (
-    "\\<emoji placeholder\\> *{description}* \n"
+    "{icon} *{description}* \n"
     "Temperature: {morning_feels_like} ℃/ {day_feels_like} ℃/ {night_feels_like} ℃ \n"
     "UV Index: {uv_index} \n"
     "Humidity: {humidity}%\n"
@@ -19,6 +20,7 @@ async def today_weather(context: ContextTypes.DEFAULT_TYPE):
     today_weather_info = weather_data["today"]
 
     message = weather_today_template.format(
+        icon=ICON_ID_TO_EMOJI.get(today_weather_info["icon_id"], ICON_ID_TO_EMOJI["default"]),
         description=today_weather_info["description"].title(),
         morning_feels_like=int(today_weather_info["morning_feels_like"]),
         day_feels_like=int(today_weather_info["day_feels_like"]),
