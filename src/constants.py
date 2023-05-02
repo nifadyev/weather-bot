6f5296f42aa7079cdff9303e4752feae639244d9
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-05-02 17:44:57 +0200

    Add weather icon id to emoji mapping

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,20 @@
+from typing import Final
+
 DAILY_FORECAST_UTC_HOUR = 5
+
+ICON_ID_TO_EMOJI: Final[dict] = {
+    "01d": "☀",
+    "01n": "☀",
+    "02d": "🌤",
+    "02n": "☀",
+    "03d": "☁️",
+    "03n": "☁️",
+    "04d": "☁️",
+    "04n": "☁️",
+    "09d": "🌦",
+    "10d": "🌧",
+    "11d": "⛈",
+    "13d": "❄️",
+    "50d": "🌫",
+    "default": "🌤",
+}
