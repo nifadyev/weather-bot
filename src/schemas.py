2f9fe223275a985dd18c602ba848d4f148941300
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-09-18 17:17:50 +0300

    WIP Pydantic schemas

--- src/schemas.py
+++ src/schemas.py
@@ -4,8 +4,14 @@ from pydantic import BaseModel, Field, confloat
 
 
 class WeatherSummary(BaseModel):
+    # TODO: Parse \n in description
     description: str
+    # TODO: Need mapping or enum
     icon_id: str = Field(str, alias="icon")
+
+    # ? For all models
+    class Config:
+        allow_mutation = False
 
 
 class DailyTemperatures(BaseModel):
@@ -15,10 +21,17 @@ class DailyTemperatures(BaseModel):
 
 
 class BaseWeather(BaseModel):
+    # ? Transform to str based on value
     clouds: int
     humidity: int
     uv_index: int = Field(int, alias="uvi")
+    # ? conlist(WeatherSummary, min_items=1, max_items=1) list with only 1 item
     weather: list[WeatherSummary]
+    # TODO: convert to str like north, east-south, etc
+    # In degrees, need to convert to south/north, etc based on it
+    # Or just add warning if strong wind with bad destination
+    # ? or conint(0, 360)
+    # ? expect str and in validator convert to int and check ranges
     wind_direction: int = Field(int, alias="wind_deg")
     wind_speed: int
 
@@ -39,6 +52,8 @@ class DailyWeather(BaseWeather):
 class Alert(BaseModel):
     event: str
     description: str
+    # TODO: check offset from UTC
+    # ? Or just make field duration (smth like repr in Django)
     start_time: datetime = Field(datetime, alias="start")
     end_time: datetime = Field(datetime, alias="end")
 
