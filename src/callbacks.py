ee6243e5ad0b690324b819de0355c69bfa0f6ad6
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-09-21 15:44:47 +0300

    Adapt function to Pydantic model

--- src/callbacks.py
+++ src/callbacks.py
@@ -1,6 +1,5 @@
 from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
-from telegram.helpers import escape_markdown
 
 from constants import ICON_ID_TO_EMOJI, TODAY_WEATHER_TEMPLATE
 from weather_services import retrieve_weather_info
@@ -8,21 +7,24 @@ from weather_services import retrieve_weather_info
 
 async def today_weather(context: ContextTypes.DEFAULT_TYPE):
     weather_data = await retrieve_weather_info()
-    today_weather_info = weather_data["today"]
+    today_weather_info = weather_data.daily[0]
 
     message = TODAY_WEATHER_TEMPLATE.format(
-        icon=ICON_ID_TO_EMOJI.get(today_weather_info["icon_id"], ICON_ID_TO_EMOJI["default"]),
-        description=today_weather_info["description"].title(),
-        morning_feels_like=int(today_weather_info["morning_feels_like"]),
-        day_feels_like=int(today_weather_info["day_feels_like"]),
-        night_feels_like=int(today_weather_info["night_feels_like"]),
-        uv_index=escape_markdown(str(today_weather_info["uv_index"]), version=2),
-        humidity=today_weather_info["humidity"],
-        wind_speed=escape_markdown(str(today_weather_info["wind_speed"]), version=2),
-        alerts_count=len(weather_data["alerts"]),
+        icon=ICON_ID_TO_EMOJI.get(
+            today_weather_info.weather[0].icon_id, ICON_ID_TO_EMOJI["default"]
+        ),
+        morning_feels_like=today_weather_info.feels_like.morning,
+        day_feels_like=today_weather_info.feels_like.day,
+        night_feels_like=today_weather_info.feels_like.night,
+        description=today_weather_info.summary,
+        uv_index=today_weather_info.uv_index,
+        humidity=today_weather_info.humidity,
+        wind_speed=today_weather_info.wind_speed,
+        alerts_count=len(weather_data.alerts),
         first_alert_description=(
-            f': {weather_data["alerts"][0]["description"]}'.title()
-            if weather_data["alerts"]
+            # TODO: concat alert summary and description because description could be empty
+            f": {weather_data.alerts[0].description}".title()
+            if weather_data.alerts
             else ""
         ),
     )
