65b2ba5cec682ea37e64291a3cb26ab7b137f322
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-03-12 14:46:33 +0300

    Add start message constant

---
+++ src/constants/messages.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+START_MESSAGE: Final[str] = """
+👋 Hi, this is Weather bot
+
+I provide actual (so called "feels" like") weather forecast by request or by periodic messages. You can customize schedule and location.
+
+I am in beta so please do not expect seamless and bug-free experience
+"""
