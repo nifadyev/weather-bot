549d2303dd297b7fa79245b52440da0711bb7b48
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-03-12 14:47:11 +0300

    Fix pytest test discovery

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,13 +1,17 @@
 import pytest
+from src.constants.messages import START_MESSAGE
+from telethon.events import StopPropagation
+from src.main import start
 
-from src.main import echo
 from tests.fixtures import EventFactory, MessageFactory
 
-# ! WIP
+
+# ! WIP - not working because EventFactory has no chat attr
 @pytest.mark.asyncio
-async def test_echo(event_loop):
+async def test_start(event_loop):
     message = MessageFactory.build()
     event = EventFactory.build(message=message)
 
     # .respond() returns None because there is no event.client
-    assert await echo(event) == event.text
+    with pytest.raises(StopPropagation):
+        assert await start(event) == START_MESSAGE
