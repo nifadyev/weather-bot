cf6a97334339ac93863dfaef1dfff14ee4f7c9a0
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-03-28 15:04:07 +0300

    Move OWM response fragment to constants

--- tests/integration/test_schemas__integration.py
+++ tests/integration/test_schemas__integration.py
@@ -6,7 +6,7 @@ from src.schemas import (
     OWMWeather,
     WeatherSummary,
 )
-
+from tests.constants import OWM_RESPONSE_FRAGMENT
 
 def test_daily_temperatures():
     raw_json = '{"day": 6.35, "eve": 1.04, "morn": -0.13, "night": -2.47}'
@@ -93,77 +93,9 @@ def test_daily_weather():
     assert DailyWeather.from_json(raw_json) == expected_schema
 
 
+# For some reason fake_owmweather is not equal to expected_schema
 def test_owmweather():
     """Create `OWMWeather` dataclass from actual API response fragment."""
-    raw_json = """
-    {
-        "alerts": [
-            {
-                "description": "Black ice",
-                "end": 1711598400,
-                "event": "Other dangers",
-                "sender_name": "web@mecom.ru",
-                "start": 1711522800,
-                "tags": [
-                    "Other dangers"
-                ]
-            }
-        ],
-        "current": {
-            "clouds": "0",
-            "dew_point": -6.46,
-            "dt": 1711022278,
-            "feels_like": 5.59,
-            "humidity": 34,
-            "pressure": 1024,
-            "sunrise": 1710990175,
-            "sunset": 1711034353,
-            "temp": 7.57,
-            "uvi": 0.61,
-            "visibility": 10000,
-            "weather": [{"description": "Clear", "icon": "01d", "id": 800, "main": "Clear"}],
-            "wind_deg": 230,
-            "wind_speed": 3
-        },
-        "daily": [
-            {
-                "clouds": 60,
-                "dew_point": -0.58,
-                "dt": 1711011600,
-                "feels_like": {"day": 2.31, "eve": 1.04, "morn": -2.25, "night": -3.5},
-                "humidity": 73,
-                "moon_phase": 0.38,
-                "moonrise": 1711017360,
-                "moonset": 1710988860,
-                "pop": 0,
-                "pressure": 1026,
-                "summary": "Expect a day of partly cloudy with clear spells",
-                "sunrise": 1710990175,
-                "sunset": 1711034353,
-                "temp": {
-                    "day": 3.73,
-                    "eve": 3.45,
-                    "max": 7.57,
-                    "min": -0.54,
-                    "morn": -0.38,
-                    "night": -1.42
-                },
-                "uvi": 1.32,
-                "weather": [
-                    {
-                        "description": "Light rain",
-                        "icon": "04d",
-                        "id": 803,
-                        "main": "Clouds"
-                    }
-                ],
-                "wind_deg": 254,
-                "wind_gust": 4.76,
-                "wind_speed": 2.86
-            }
-        ]
-    }
-    """
     current_weather = CurrentWeather(
         uv_index=0,
         weather=[WeatherSummary(description="Clear", icon_id="01d")],
@@ -187,7 +119,7 @@ def test_owmweather():
         current=current_weather, daily=[daily_weather], alerts=[alert]
     )
 
-    assert OWMWeather.from_json(raw_json) == expected_schema
+    assert OWMWeather.from_json(OWM_RESPONSE_FRAGMENT) == expected_schema
 
 
 def test_owmweather_default_alerts():
