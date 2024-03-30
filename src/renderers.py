50bfe334b68504851233b38ae1fea27a782815b9
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-03-30 10:19:02 +0300

    Add renderer for current forecast template

---
+++ src/renderers.py
@@ -0,0 +1,26 @@
+from constants.messages import CURRENT_FORECAST_TEMPLATE_EN, ICON_ID_TO_EMOJI
+from schemas import OWMWeather
+
+
+def render_alerts(alerts) -> str:
+    """Usually, only first item is localized."""
+    if alerts and (alert_description := alerts[0].description):
+        return f", {alert_description.lower()}"
+
+    return ""
+
+def render_current_forecast(forecast: OWMWeather) -> str:
+    today_forecast = forecast.daily[0]
+
+    return CURRENT_FORECAST_TEMPLATE_EN.format(
+        icon=ICON_ID_TO_EMOJI.get(
+            today_forecast.weather[0].icon_id, ICON_ID_TO_EMOJI["default"]
+        ),
+        summary=today_forecast.summary,
+        current_temperature=forecast.current.feels_like,
+        current_summary=forecast.current.weather[0].description,
+        morning_temperature=today_forecast.temp.morning,
+        day_temperature=today_forecast.temp.day,
+        night_temperature=today_forecast.temp.night,
+        alert=render_alerts(forecast.alerts),
+    )
